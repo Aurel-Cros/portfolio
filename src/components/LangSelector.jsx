486169d47dd9d139ae7ec239cad5fbff1f45a897
Aurélien Cros <aure.cros@gmail.com>
2023-07-19 11:41:06 +0200

    feat/dev : Navbar
    Complete navbar with animations

--- src/components/LangSelector.jsx
+++ src/components/LangSelector.jsx
@@ -8,8 +8,8 @@ export default function LangSelector() {
 
     return (
         <div className={"lang-selector" + (lang === 'en' ? ' en' : '')}>
-            <img onClick={() => { switchLang('fr') }} src="./assets/fr-flag.svg"></img>
-            <img onClick={() => { switchLang('en') }}  src="./assets/uk-flag.svg"></img>
+            <img onClick={() => { switchLang('fr') }} src="./assets/icons/fr-flag.svg"></img>
+            <img onClick={() => { switchLang('en') }}  src="./assets/icons/uk-flag.svg"></img>
             <div className="background"></div>
         </div>
     )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,12 +4,29 @@ import LangSelector from './LangSelector.jsx';
 export default function NavBar() {
     return (
         <div className="navbar">
-            <LangSelector />
-            <div className="socials">
-                <p>Find me</p>
-                <img alt="GitHub link" src="./assets/icons/github.svg"></img>
-                <img alt="LinkedIn link" src="./assets/icons/linkedin.svg"></img>
+            <div className="navWrapper">
+                <LangSelector />
+                <div className="socials">
+                    <p className="nav">Find me</p>
+                    <div>
+                        <a href="https://www.github.com/Aurel-Cros" target="_blank">
+                            <img alt="GitHub link" src="./assets/icons/github-white.svg"></img>
+                        </a>
+                        <a href="https://www.linkedin.com/in/aurel-cros/" target="_blank">
+                            <img alt="LinkedIn link" src="./assets/icons/linkedin.svg"></img>
+                        </a>
+                    </div>
+                </div>
+                <div className="navLinks">
+                    <a href="#" className="nav about">About me</a>
+                    <a href="#" className="nav work">My work</a>
+                    <a href="#" className="nav contact">Get in touch</a>
+                </div>
             </div>
+            <button className="navSwitch" onClick={() => { document.querySelector('.navbar').classList.toggle('closed') }}>
+                <span></span>
+                <span></span>
+            </button>
         </div>
     )
 }
