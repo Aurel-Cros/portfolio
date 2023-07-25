60967541177b00fee234e9e574ef57f12334eba6
Aurélien Cros <aure.cros@gmail.com>
2023-07-25 09:41:24 +0200

    fix/dev : Back link
    Removed useless variable declarations

--- src/components/BackArrow.jsx
+++ src/components/BackArrow.jsx
@@ -2,7 +2,7 @@ import { Link, useLocation } from "react-router-dom";
 
 export default function () {
     const location = useLocation();
-    const { hash, pathname, search } = location;
+    const { pathname } = location;
     const target = /\/project\/(?:.+)/.test(pathname) ? "/my-work" : "/"
     console.log(pathname)
     return pathname !== "/" ? (
