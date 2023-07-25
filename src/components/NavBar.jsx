987f73afe94d0211d3eab1864939d92f590dac6e
Aurélien Cros <aure.cros@gmail.com>
2023-07-25 10:56:36 +0200

    feat/dev : content
    Added the content manager function to return the proper strings according to current language

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 import LangSelector from './LangSelector.jsx';
 import { Link } from 'react-router-dom';
+import getContent from '../utils/contentManager.js';
 
 export default function NavBar() {
+    const content = getContent('navbar');
     return (
         <div className="navbar">
             <div className="navWrapper">
                 <LangSelector />
                 <div className="socials">
-                    <p className="nav">Find me</p>
+                    <p className="nav">{content.find_me}</p>
                     <div>
                         <a href="https://www.github.com/Aurel-Cros" target="_blank">
                             <img alt="GitHub link" src="./assets/icons/github-white.svg"></img>
@@ -18,9 +20,9 @@ export default function NavBar() {
                     </div>
                 </div>
                 <div className="navLinks">
-                    <Link to="/about-me" className="nav about">About me</Link>
-                    <Link to="/my-work" className="nav work">My work</Link>
-                    <Link to="/contact" className="nav contact">Get in touch</Link>
+                    <Link to="/about-me" className="nav about">{content.About}</Link>
+                    <Link to="/my-work" className="nav work">{content.Work}</Link>
+                    <Link to="/contact" className="nav contact">{content.Contact}</Link>
                 </div>
             </div>
             <button className="navSwitch" onClick={() => { document.querySelector('.navbar').classList.toggle('closed') }}>
