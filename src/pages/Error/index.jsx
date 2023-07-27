f5723f4ce28cc712d8750f376accdca941ddbe61
Aurélien Cros <aure.cros@gmail.com>
2023-07-27 09:26:19 +0200

    feat/dev : error page
    Styled error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,7 +1,21 @@
+import getContent from "../../utils/contentManager";
+import styled from 'styled-components';
+import * as mixins from '../../styles/mixins';
+
+const ErrorFrame = styled.div`
+    ${mixins.glassmorph.medium};
+    ${mixins.blocks.bdradius};
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+`
 export default function Error() {
+    const content = getContent().error;
     return (
-        <div>
-            <h1>Oups 🙈 Cette page n'existe pas</h1>
-        </div>
+        <ErrorFrame>
+            <h1>{content.e404}</h1>
+        </ErrorFrame>
     )
 }
