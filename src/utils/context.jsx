4d1f8678ced45a0121443dc94360deedd349ab81
Aurélien Cros <aure.cros@gmail.com>
2023-09-01 13:46:51 +0200

    feat/dev : lang
    Added localStorage saving and loading for lang setting

--- src/utils/context.jsx
+++ src/utils/context.jsx
@@ -2,8 +2,12 @@ import { createContext, useState } from 'react';
 export const LangContext = createContext();
 
 export const LangProvider = ({ children }) => {
-    const [lang, setLang] = useState('fr');
-
+    const [lang, setLangState] = useState(
+        localStorage.getItem('lang') || 'fr');
+    const setLang = (value) => {
+        setLangState(value);
+        localStorage.setItem('lang', value);
+    }
     return (
         <LangContext.Provider value={{ lang, setLang }}>
             {children}
